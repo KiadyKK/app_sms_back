d9e4fb788a7af0f1e97d44452429b25a3ad9561d
RAMANJATO Kiadinirina Biaza <biaza.ramanjato@orange.com>
2024-03-08 19:49:16 +0300

    initialize project

---
+++ src/main/java/org/acme/domain/entities/Rdz.java
@@ -0,0 +1,24 @@
+package org.acme.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "rdz")
+public class Rdz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String tri;
+
+    @Column
+    private String nom;
+
+    @Column
+    private String prenom;
+}
