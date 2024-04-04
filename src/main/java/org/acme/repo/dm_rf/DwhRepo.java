d44ca9a13ce191b38f55815b15373dd8792fe0ce
Andriary <andryantenaina29@gmail.com>
2024-04-04 12:02:08 +0300

    commit

--- src/main/java/org/acme/repo/dm_rf/DwhRepo.java
+++ src/main/java/org/acme/repo/dm_rf/DwhRepo.java
@@ -35,8 +35,6 @@ public class DwhRepo {
                 "\tSUM(mtt_rec) mtt_rec,\n" +
                 "\tSUM(cumul_mtt_rec) cumul_mtt_rec\n" +
                 "FROM (\n" +
-                "\t###### PARC ######\n" +
-                "\t#parc_journalier\n" +
                 "\tSELECT  \n" +
                 "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
                 "\t\tupd_dt,\n" +
@@ -65,8 +63,6 @@ public class DwhRepo {
                 "\t\n" +
                 "\tUNION ALL\n" +
                 "\t\n" +
-                "\t###### ACTIVATION ######\n" +
-                "\t#activation_journalier\n" +
                 "\tSELECT  \n" +
                 "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
                 "\t\tupd_dt,\n" +
@@ -93,7 +89,6 @@ public class DwhRepo {
                 "\t\n" +
                 "\tUNION ALL\n" +
                 "\t\n" +
-                "\t#Cumul_activation\n" +
                 "\tSELECT  \n" +
                 "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
                 "\t\tMAX(upd_dt),\n" +
@@ -120,8 +115,6 @@ public class DwhRepo {
                 "\t\n" +
                 "\tUNION ALL \n" +
                 "\t\n" +
-                "\t###### RECHARGEMENT ######\n" +
-                "\t#Rechargement journalier\n" +
                 "\tSELECT \n" +
                 "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
                 "\t\trec.upd_dt upd_dt,\n" +
@@ -160,7 +153,6 @@ public class DwhRepo {
                 "\t\n" +
                 "\tUNION ALL\n" +
                 "\n" +
-                "\t#Cumul Rechargement\n" +
                 "\tSELECT \n" +
                 "\t\tcumul_rec.periode mois_annee,\n" +
                 "\t\tcumul_rec.upd_dt upd_dt,\n" +
@@ -201,7 +193,6 @@ public class DwhRepo {
                 "\t\t\n" +
                 "\t\tUNION ALL\n" +
                 "\t\t\n" +
-                "\t#Charged Base\n" +
                 "\tSELECT\n" +
                 "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
                 "\t\tupd_dt,\n" +
@@ -235,180 +226,6 @@ public class DwhRepo {
                 ")users*/\n" +
                 "GROUP BY jour, /*users.user_phone,*/ zone \n" +
                 ";";
-        String query = "SELECT " +
-                "mois_annee," +
-                "upd_dt jour," +
-                "zone," +
-                "SUM(parc) 'parc'," +
-                "SUM(activation) activation," +
-                "SUM(cumul_activation) cumul_activation," +
-                "SUM(cb_30jours) cb_30jours,"+
-                "SUM(cb_7jours) cb_7jours," +
-                "SUM(cb_30jours_data) cb_30jours_data," +
-                "SUM(nb_rec) nb_rec," +
-                "SUM(cumul_nb_rec) cumul_nb_rec," +
-                "SUM(mtt_rec) mtt_rec," +
-                "SUM(cumul_mtt_rec) cumul_mtt_rec " +
-                "FROM (" +
-                "SELECT " +
-                "DATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee," +
-                "upd_dt," +
-                "CASE " +
-                "WHEN rf.sig_zoneorange_name_v3 IS NULL THEN 'INCONNU' " +
-                "ELSE rf.sig_zoneorange_name_v3 " +
-                "END AS zone," +
-                "SUM(qty) parc," +
-                "0 activation," +
-                "0 cumul_activation," +
-                "0 nb_rec," +
-                "0 cumul_nb_rec," +
-                "0 mtt_rec," +
-                "0 cumul_mtt_rec " +
-                "FROM  DM_OD.od_parc_orange od " +
-                "LEFT JOIN DM_RF.rf_customer_group cg ON od.prg_code=cg.prgcode AND cg.type ='Facturable' " +
-                "LEFT JOIN DM_RF.`rf_sig_cell_krill_v3` rf ON od.site_id =rf.sig_id " +
-                "WHERE upd_dt = '" + endDate + "' " +
-                "AND statut IN (1,2,5) " +
-                "AND parc_id = 1 " +
-                "AND billing_type IN(1,5) " +
-                "GROUP BY upd_dt, zone " +
-                "UNION ALL " +
-                "SELECT " +
-                "DATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee," +
-                "upd_dt," +
-                "CASE " +
-                "WHEN rf.sig_zoneorange_name_v3 IS NULL THEN 'INCONNU' " +
-                "ELSE rf.sig_zoneorange_name_v3 " +
-                "END AS zone," +
-                "0 parc," +
-                "SUM(IF(parc_id=1,qty,0)) activation," +
-                "0 cumul_activation," +
-                "0 nb_rec," +
-                "0 cumul_nb_rec," +
-                "0 mtt_rec," +
-                "0 cumul_mtt_rec " +
-                "FROM  DM_OD.od_parc_orange od " +
-                "LEFT JOIN DM_RF.`rf_sig_cell_krill_v3` rf ON od.site_id = rf.sig_id " +
-                "WHERE upd_dt = '" + endDate + "' " +
-                "AND billing_type IN (1,5) " +
-                "AND statut = 1 " +
-                "GROUP BY upd_dt, zone " +
-                "UNION ALL " +
-                "SELECT " +
-                "DATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee," +
-                "MAX(upd_dt)," +
-                "CASE " +
-                "WHEN rf.sig_zoneorange_name_v3 IS NULL THEN 'INCONNU' " +
-                "ELSE rf.sig_zoneorange_name_v3 " +
-                "END AS zone," +
-                "0 parc," +
-                "0 activation," +
-                "SUM(IF(parc_id=1,qty,0)) cumul_activation," +
-                "0 nb_rec," +
-                "0 cumul_nb_rec," +
-                "0 mtt_rec," +
-                "0 cumul_mtt_rec " +
-                "FROM  DM_OD.od_parc_orange od " +
-                "LEFT JOIN DM_RF.`rf_sig_cell_krill_v3` rf ON od.site_id = rf.sig_id " +
-                "WHERE upd_dt BETWEEN '" + startDate + "' AND '" + endDate + "' " +
-                "AND billing_type IN (1,5) " +
-                "AND statut = 1 " +
-                "GROUP BY mois_annee, zone " +
-                "UNION ALL " +
-                "SELECT " +
-                "DATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee," +
-                "upd_dt," +
-                "CASE " +
-                "WHEN rf.sig_zoneorange_name_v3 IS NULL THEN 'INCONNU'" +
-                "ELSE rf.sig_zoneorange_name_v3 " +
-                "END AS zone," +
-                "0 parc," +
-                "0 activation," +
-                "0 cumul_activation," +
-                "SUM(qty2) cb_30jours," +
-                "SUM(qty4) cb_7jours," +
-                "SUM(qty3) cb_30jours_data," +
-                "0 nb_rec," +
-                "0 cumul_nb_rec," +
-                "0 mtt_rec," +
-                "0 cumul_mtt_rec " +
-                "FROM DM_OD.od_parc_orange od " +
-                "LEFT JOIN DM_RF.`rf_sig_cell_krill_v3` rf ON od.site_id =rf.sig_id " +
-                "WHERE upd_dt = '" + endDate + "' " +
-                "AND statut <> '4' " +
-                "AND billing_type = '1' " +
-                "GROUP BY upd_dt, zone " +
-                "UNION ALL " +
-                "SELECT " +
-                "DATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee," +
-                "rec.upd_dt upd_dt," +
-                "rec.zone zone," +
-                "0 parc," +
-                "0 activation," +
-                "0 cumul_activation," +
-                "(rec.qty_scratch+(rec.qty_erecharge+rec.qty_om)) nb_rec," +
-                "0 cumul_nb_rec," +
-                "(rec.achat_scratch+(rec.achat_erecharge+rec.achat_om)) mtt_rec," +
-                "0 cumul_mtt_rec " +
-                "FROM " +
-                "(" +
-                "SELECT " +
-                "upd_dt," +
-                "SUM(IF(rf.id IN (1,3), qty, 0)) qty_scratch," +
-                "SUM(IF(rf.id IN (1,3), (amount*qty)/1.2, 0)) achat_scratch," +
-                "SUM(IF(rf.id IN(2,5,6,10,11), qty, 0)) qty_erecharge," +
-                "SUM(IF(rf.id IN (2,5,6,10,11),(amount*qty)/1.2, 0)) achat_erecharge," +
-                "SUM(IF(rf.id = 4,qty, 0)) qty_om," +
-                "SUM(IF(rf.id = 4, (amount*qty)/1.2, 0)) achat_om," +
-                "SUM(fees) fees," +
-                "CASE " +
-                "WHEN sg.sig_zoneorange_name_v3 IS NULL THEN 'INCONNU' " +
-                "ELSE sg.sig_zoneorange_name_v3 " +
-                "END AS zone " +
-                "FROM DM_OD.od_recharge c " +
-                "INNER JOIN DM_RF.`rf_recharge_code` rc ON rc.code = c.channel_id " +
-                "INNER JOIN DM_RF.rf_rec_type rf  ON rf.id = rc.rec_type " +
-                "LEFT JOIN DM_RF.rf_sig_cell_krill_v3 sg ON sg.sig_lac_ci=c.cell_id " +
-                "WHERE upd_dt = '" + endDate + "' GROUP BY upd_dt, zone " +
-                ")rec " +
-                "UNION ALL " +
-                "SELECT " +
-                "cumul_rec.periode mois_annee," +
-                "cumul_rec.upd_dt upd_dt," +
-                "cumul_rec.zone zone," +
-                "0 parc," +
-                "0 activation," +
-                "0 cumul_activation," +
-                "0 nb_rec," +
-                "(cumul_rec.qty_scratch+(cumul_rec.qty_erecharge+cumul_rec.qty_om)) cumul_nb_rec," +
-                "0 mtt_rec," +
-                "(cumul_rec.achat_scratch+(cumul_rec.achat_erecharge+cumul_rec.achat_om)) cumul_mtt_rec " +
-                "FROM " +
-                "(" +
-                "SELECT " +
-                "DATE_FORMAT(upd_dt, '%m-%Y') periode," +
-                "MAX(upd_dt) upd_dt," +
-                "SUM(IF(rf.id IN (1,3), qty, 0)) qty_scratch," +
-                "SUM(IF(rf.id IN (1,3), (amount*qty)/1.2, 0)) achat_scratch," +
-                "SUM(IF(rf.id IN(2,5,6,10,11), qty, 0)) qty_erecharge, " +
-                "SUM(IF(rf.id IN (2,5,6,10,11),(amount*qty)/1.2, 0)) achat_erecharge," +
-                "SUM(IF(rf.id = 4,qty, 0)) qty_om," +
-                "SUM(IF(rf.id = 4, (amount*qty)/1.2, 0)) achat_om," +
-                "SUM(fees) fees," +
-                "CASE " +
-                "WHEN sg.sig_zoneorange_name_v3 IS NULL THEN 'INCONNU' " +
-                "ELSE sg.sig_zoneorange_name_v3 " +
-                "END AS zone " +
-                "FROM DM_OD.od_recharge c " +
-                "INNER JOIN DM_RF.`rf_recharge_code` rc ON rc.code = c.channel_id " +
-                "INNER JOIN DM_RF.rf_rec_type rf  ON rf.id = rc.rec_type " +
-                "LEFT JOIN DM_RF.rf_sig_cell_krill_v3 sg ON sg.sig_lac_ci=c.cell_id " +
-//                "WHERE upd_dt BETWEEN '" + startDate + "' AND '" + endDate + "' " +
-                "WHERE upd_dt BETWEEN '" + startDate + "' AND '" + endDate + "' " +
-                "GROUP BY zone " +
-                ")cumul_rec " +
-                ") table_finale " +
-                "GROUP BY jour, zone";
 
         try(Connection conn = dataSource.getConnection();
             PreparedStatement stmt = conn.prepareStatement(query1)) {
