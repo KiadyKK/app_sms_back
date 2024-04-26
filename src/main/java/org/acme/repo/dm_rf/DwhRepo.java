fb85ec4f5cc54b89beabd2ff13d39c06b9b93f22
Andriary <andryantenaina29@gmail.com>
2024-04-26 11:53:13 +0300

    changement requete

--- src/main/java/org/acme/repo/dm_rf/DwhRepo.java
+++ src/main/java/org/acme/repo/dm_rf/DwhRepo.java
@@ -204,10 +204,183 @@ public class DwhRepo {
                 "upd_dt, zone) table_finale\n" +
                 "GROUP BY jour, zone";
 
+        String query1 = "SELECT\n" +
+                "\tmois_annee,\n" +
+                "\tupd_dt jour,\n" +
+                "\tzone,\n" +
+                "\tSUM(parc) parc,\n" +
+                "\tSUM(delta_parc) delta_parc,\n" +
+                "\tSUM(activation) activation,\n" +
+                "\tSUM(cumul_activation) cumul_activation,  \n" +
+                "\tSUM(cb_30jours) cb_30j,\n" +
+                "\tSUM(cb_7jours) cb_7j, \n" +
+                "\tSUM(cb_30jours_data) cb_30jd,\n" +
+                "\tSUM(mtt_rec) mtt_rec,\n" +
+                "\tSUM(cumul_mtt_rec) cumul_mtt_rec\n" +
+                "FROM (\n" +
+                "\tSELECT  \n" +
+                "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
+                "\t\tupd_dt,\n" +
+                "\t\tzone,\n" +
+                "\t\tSUM(qty_j_1) parc,\n" +
+                "\t\tdelta delta_parc,\n" +
+                "\t\t0 activation,\n" +
+                "\t\t0 cumul_activation,  \n" +
+                "\t\t0 cb_30jours,\n" +
+                "\t\t0 cb_7jours, \n" +
+                "\t\t0 cb_30jours_data,\n" +
+                "\t\t0 mtt_rec,\n" +
+                "\t\t0 cumul_mtt_rec\n" +
+                "\tFROM(\n" +
+                "\t\tSELECT \n" +
+                "\t\t\t"+ endDate +" upd_dt, \n" +
+                "\t\t\tSUM(IF(upd_dt='2024-04-23',qty,0)) qty_j_1 ,\n" +
+                "\t\t\tSUM(IF(upd_dt=DATE_SUB('"+ endDate +"', INTERVAL 1 DAY),qty,0)) qty_j_2, \n" +
+                "\t\t\tIF(v3.sig_zoneorange_name_v3 IS NULL,'INCONNU',v3.sig_zoneorange_name_v3) zone,\n" +
+                "\t\t\tSUM(IF(upd_dt='"+ endDate +"',qty,0))-SUM(IF(upd_dt=DATE_SUB('"+ endDate +"', INTERVAL 1 DAY),qty,0)) delta\n" +
+                "\t\tFROM DM_OD.od_parc_orange od\n" +
+                "\t\tLEFT JOIN DM_RF.`rf_sig_cell_krill_v3` v3 ON v3.sig_lac_ci=od.site_id\n" +
+                "\t\tWHERE upd_dt BETWEEN  DATE_SUB('"+ endDate +"', INTERVAL 1 DAY) AND '"+ endDate +"' \n" +
+                "\t\t\t\t\tAND parc_id = 5\n" +
+                "\t\t\t\t\tAND billing_type IN (1)\n" +
+                "\t\t\t\t\tAND statut IN (1,2,5)\n" +
+                "\t\t\t\t\tGROUP BY v3.sig_zoneorange_name_v3\n" +
+                "                \n" +
+                "        ) t\n" +
+                "\tGROUP BY upd_dt, zone\n" +
+                "\t\n" +
+                "\tUNION ALL\n" +
+                "\t\n" +
+                "\tSELECT  \n" +
+                "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
+                "\t\tupd_dt,\n" +
+                "\t\tIF(rf.sig_zoneorange_name_v3 IS NULL,'INCONNU',rf.sig_zoneorange_name_v3) zone,\n" +
+                "\t\t0 parc,\n" +
+                "\t\t0 delta_parc,\n" +
+                "\t\tSUM(IF(parc_id=1,qty,0)) activation,\n" +
+                "\t\t0 cumul_activation,  \n" +
+                "\t\t0 cb_30jours,\n" +
+                "\t\t0 cb_7jours, \n" +
+                "\t\t0 cb_30jours_data,\n" +
+                "\t\t0 mtt_rec,\n" +
+                "\t\t0 cumul_mtt_rec\n" +
+                "\tFROM  DM_OD.od_parc_orange od \n" +
+                "\tLEFT JOIN DM_RF.`rf_sig_cell_krill_v3` rf ON od.site_id =rf.sig_lac_ci\n" +
+                "\tWHERE upd_dt = '"+ endDate +"'\n" +
+                "\tAND billing_type IN (1,5)\n" +
+                "\tAND statut = 1\n" +
+                "\tGROUP BY upd_dt, zone\n" +
+                "\t\n" +
+                "\tUNION ALL\n" +
+                "\t\n" +
+                "\tSELECT  \n" +
+                "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
+                "\t\tMAX(upd_dt),\n" +
+                "\t\tIF(rf.sig_zoneorange_name_v3 IS NULL,'INCONNU',rf.sig_zoneorange_name_v3) zone,\n" +
+                "\t\t0 parc,\n" +
+                "\t\t0 delta_parc,\n" +
+                "\t\t0 activation,\n" +
+                "\t\tSUM(IF(parc_id=1,qty,0)) cumul_activation,  \n" +
+                "\t\t0 cb_30jours,\n" +
+                "\t\t0 cb_7jours, \n" +
+                "\t\t0 cb_30jours_data,\n" +
+                "\t\t0 mtt_rec,\n" +
+                "\t\t0 cumul_mtt_rec\n" +
+                "\tFROM  DM_OD.od_parc_orange od \n" +
+                "\tLEFT JOIN DM_RF.`rf_sig_cell_krill_v3` rf ON od.site_id =rf.sig_lac_ci\n" +
+                "\tWHERE upd_dt BETWEEN '"+ startDate +"' AND '"+ endDate +"'\n" +
+                "\tAND billing_type IN (1,5)\n" +
+                "\tAND statut = 1\n" +
+                "\tGROUP BY mois_annee, zone\n" +
+                "\t\n" +
+                "\tUNION ALL \n" +
+                "\t\n" +
+                "\tSELECT \n" +
+                "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
+                "\t\trec.upd_dt upd_dt,\n" +
+                "\t\trec.zone zone,\n" +
+                "\t\t0 parc,\n" +
+                "\t\t0 delta_parc,\n" +
+                "\t\t0 activation,\n" +
+                "\t\t0 cumul_activation,  \n" +
+                "\t\t0 cb_30jours,\n" +
+                "\t\t0 cb_7jours, \n" +
+                "\t\t0 cb_30jours_data,\n" +
+                "\t\tSUM(amnt) mtt_rec,\n" +
+                "\t\t0 cumul_mtt_rec\n" +
+                "\tFROM\t\n" +
+                "\t\t(\n" +
+                "\t\t\tSELECT \n" +
+                "\t\t\t\tupd_dt, \n" +
+                "\t\t\t\tIFNULL(sg.sig_zoneorange_name_v3,'INCONNU') zone, \n" +
+                "\t\t\t\tSUM(amount_htva) amnt\n" +
+                "\t\t\tFROM `DM_CB`.`cb_recharge_qs` qs\n" +
+                "\t\t\tLEFT JOIN DM_RF.rf_sig_cell_krill_v3 sg ON sg.sig_cel=qs.cel AND sg.sig_lac=qs.lac\n" +
+                "\t\t\tWHERE upd_dt = '"+ endDate +"' AND billing_type = 1\n" +
+                "\t\t\tGROUP BY upd_dt, sg.sig_zoneorange_name_v3\n" +
+                "\t\t)rec \n" +
+                "\t\tGROUP BY zone\n" +
+                "\t\t\n" +
+                "\tUNION ALL\n" +
+                "\n" +
+                "\tSELECT \n" +
+                "\t\tcumul_rec.periode mois_annee,\n" +
+                "\t\tcumul_rec.upd_dt upd_dt,\n" +
+                "\t\tcumul_rec.zone zone,\n" +
+                "\t\t0 parc,\n" +
+                "\t\t0 delta_parc,\n" +
+                "\t\t0 activation,\n" +
+                "\t\t0 cumul_activation,  \n" +
+                "\t\t0 cb_30jours,\n" +
+                "\t\t0 cb_7jours, \n" +
+                "\t\t0 cb_30jours_data,\n" +
+                "\t\t0 mtt_rec,\n" +
+                "\t\tSUM(amnt) cumul_mtt_rec\n" +
+                "\tFROM\t\n" +
+                "\t\t(\n" +
+                "\t\t\tSELECT \n" +
+                "\t\t\t\tDATE_FORMAT(upd_dt, '%m-%Y') periode,\n" +
+                "\t\t\t\tMAX(upd_dt) upd_dt, \n" +
+                "\t\t\t\tIFNULL(sg.sig_zoneorange_name_v3,'INCONNU') zone, \n" +
+                "\t\t\t\tSUM(amount_htva) amnt\n" +
+                "\t\t\tFROM `DM_CB`.`cb_recharge_qs` qs\n" +
+                "\t\t\tLEFT JOIN DM_RF.rf_sig_cell_krill_v3 sg ON sg.sig_cel=qs.cel AND sg.sig_lac=qs.lac\n" +
+                "\t\t\tWHERE upd_dt BETWEEN  '"+ startDate +"' AND '"+ endDate +"' AND billing_type = 1\n" +
+                "\t\t\tGROUP BY periode, sg.sig_zoneorange_name_v3 \n" +
+                "\t\t)cumul_rec\n" +
+                "\tGROUP BY zone\n" +
+                "\t\t\n" +
+                "\tUNION ALL\n" +
+                "\t\t\n" +
+                "\t#Charged Base\n" +
+                "\tSELECT\n" +
+                "\t\tDATE_FORMAT(upd_dt, \"%m-%Y\") mois_annee,\n" +
+                "\t\tupd_dt,\n" +
+                "\t\tIF(rf.sig_zoneorange_name_v3 IS NULL,'INCONNU',rf.sig_zoneorange_name_v3) zone,\n" +
+                "\t\t0 parc,\n" +
+                "\t\t0 delta_parc,\n" +
+                "\t\t0 activation,\n" +
+                "\t\t0 cumul_activation,  \n" +
+                "\t\tSUM(qty2) cb_30jours,\n" +
+                "\t\tSUM(qty4) cb_7jours, \n" +
+                "\t\tSUM(qty3) cb_30jours_data,\n" +
+                "\t\t0 mtt_rec,\n" +
+                "\t\t0 cumul_mtt_rec\n" +
+                "\tFROM DM_OD.od_parc_orange od \n" +
+                "\tLEFT JOIN DM_RF.`rf_sig_cell_krill_v3` rf ON od.site_id =rf.sig_lac_ci\n" +
+                "\tWHERE upd_dt = '"+ endDate +"'\n" +
+                "\t\tAND statut <> 4\n" +
+                "\t\tAND billing_type = 1\n" +
+                "\tGROUP BY \n" +
+                "\t\tupd_dt, zone\n" +
+                "\n" +
+                ") table_finale\n" +
+                "GROUP BY jour,zone;";
+
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
+             PreparedStatement stmt = conn.prepareStatement(query1)) {
             List<DwhRes> dwhResList = new ArrayList<>();
-            try (ResultSet rs = stmt.executeQuery(query)) {
+            try (ResultSet rs = stmt.executeQuery(query1)) {
                 while (rs.next()) {
                     DwhRes dwhRes = new DwhRes();
                     dwhRes.setMois_annee(rs.getString("mois_annee"));
