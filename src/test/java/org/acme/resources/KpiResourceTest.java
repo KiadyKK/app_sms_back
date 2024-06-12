23cd95039946ade7d5ceb60e5e573a5635b1cd29
Michelle RAKOTOVAO <rakotovaomichelle7@gmail.com>
2024-06-12 11:50:27 +0300

    getZoneSuccess done

--- src/test/java/org/acme/resources/KpiResourceTest.java
+++ src/test/java/org/acme/resources/KpiResourceTest.java
@@ -1,22 +1,43 @@
 package org.acme.resources;
 import io.quarkus.test.InjectMock;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestExtension;
+import io.quarkus.test.security.TestSecurity;
+import io.restassured.http.ContentType;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 import org.acme.model.dm_rf.DwhRes;
+import org.acme.model.dm_rf.Zone;
 import org.acme.services.KpiService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.net.URL;
+import java.security.Principal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import static io.restassured.RestAssured.given;
+import static org.acme.common.Constant.APPSMS;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 @QuarkusTest
-class KpiResourceTest {
+class KpiResourceTest extends QuarkusTestExtension {
     @InjectMock
     KpiService kpiService;
+    @Inject
+    SecurityContext securityContext;
+    @TestHTTPResource("/kpi/zone")
+    URL kpiZoneUrl;
+    @BeforeEach
+    void setUp() {
+    }
     @Test
     void getDwhSuccess() {
         DwhRes dwhres= new DwhRes();
@@ -145,7 +166,27 @@ class KpiResourceTest {
         Mockito.verify(kpiService).getAllDwh(any(String.class));
     }
     @Test
-    void getZone() {
+    @TestSecurity(authorizationEnabled = false)
+    void getZoneSuccess() {
+        Zone zone=new Zone();
+        zone.setName("Alaotra");
+
+        Zone zone1=new Zone();
+        zone1.setName("Itasy");
+        List<Zone> listZone=new ArrayList<>();
+        listZone.add(zone);
+        listZone.add(zone1);
+        Mockito.when(kpiService.getZone()).thenReturn(Response.ok(listZone).type(MediaType.APPLICATION_JSON).build());
+
+        given()
+                .when().get(kpiZoneUrl)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .contentType(ContentType.JSON)
+                .body("[0].name",is(zone.getName()))
+                .body("[1].name",is(zone1.getName()));
+        Mockito.verify(kpiService).getZone();
+
     }
 
     @Test
